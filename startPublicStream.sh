53963b067e33d06f8fa22135f2d37b53a146efaa
soltiger <antti.koskenalho@gmail.com>
2016-01-24 13:06:55 +0000

    Initial commit

---
+++ startPublicStream.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pkill raspistill
+
+rm -f /home/pi/stream/pic*
+
+/usr/bin/raspistill --nopreview -w 640 -h 480 -q 5 -o /home/pi/stream/pic.jpg -tl 1 -t 9999999 -th 0:0:0 2> /dev/null &
+
+LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer -i "input_file.so -f /home/pi/stream -n pic.jpg" -o "output_http.so -w /usr/local/www -p 500"
